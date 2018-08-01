fd83ec3bc09ec2bb79774d7f38069dd043e1a0bb
Abdurrahman Shofy Adianto <azophy@gmail.com>
2018-08-01 21:22:50 +0700

    change welcome.blade.php for testing server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    UNDANGAN.MU
                 </div>
 
                 <div class="links">
