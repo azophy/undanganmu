44d7b3e7fbc1fe467f0b65d6856f414e1eff22cf
Abdurrahman Shofy Adianto <azophy@gmail.com>
2018-08-15 10:58:07 +0700

    change asset method on template to accomodate https mixed content restriction

--- resources/views/templates/undanganmu/index.blade.php
+++ resources/views/templates/undanganmu/index.blade.php
@@ -1,3 +1,11 @@
+@php
+function asset_url($param) { 
+    if (Request::secure()) 
+        return secure_asset($param); 
+    else
+        return asset($param); 
+}
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,18 +19,18 @@
     <title>Creative - Start Bootstrap Theme</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{ asset('templates/undanganmu') }}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset_url('templates/undanganmu') }}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="{{ asset('templates/undanganmu') }}/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset_url('templates/undanganmu') }}/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
     <!-- Plugin CSS -->
-    <link href="{{ asset('templates/undanganmu') }}/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="{{ asset_url('templates/undanganmu') }}/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="{{ asset('templates/undanganmu') }}/css/creative.min.css" rel="stylesheet">
+    <link href="{{ asset_url('templates/undanganmu') }}/css/creative.min.css" rel="stylesheet">
 
   </head>
 
@@ -117,8 +125,8 @@
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/1.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/1.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/1.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -132,8 +140,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/2.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/2.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/2.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -147,8 +155,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/3.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/3.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/3.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -162,8 +170,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/4.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/4.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/4.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -177,8 +185,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/5.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/5.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/5.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -192,8 +200,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ asset('templates/undanganmu') }}/img/portfolio/fullsize/6.jpg">
-              <img class="img-fluid" src="{{ asset('templates/undanganmu') }}/img/portfolio/thumbnails/6.jpg" alt="">
+            <a class="portfolio-box" href="{{ asset_url('templates/undanganmu') }}/img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="{{ asset_url('templates/undanganmu') }}/img/portfolio/thumbnails/6.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -238,16 +246,16 @@
     </section>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="{{ asset('templates/undanganmu') }}/vendor/jquery/jquery.min.js"></script>
-    <script src="{{ asset('templates/undanganmu') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
                   
     <!-- Plugin JavaScript -->
-    <script src="{{ asset('templates/undanganmu') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="{{ asset('templates/undanganmu') }}/vendor/scrollreveal/scrollreveal.min.js"></script>
-    <script src="{{ asset('templates/undanganmu') }}/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="{{ asset('templates/undanganmu') }}/js/creative.min.js"></script>
+    <script src="{{ asset_url('templates/undanganmu') }}/js/creative.min.js"></script>
 
   </body>
 
